97ec8a71e2c6d6973f7275f9ae13594a19d0affe
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-07-31 21:07:00 +0800

    use dein.toml&install coc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,11 @@
 local path_here = debug.getinfo(1).source:match("@?(.*/)")
-
-local function vscode_exists()
-    return vim.fn.exists('g:vscode') == 1
-end
-
-local function source(path)
-    vim.cmd(string.format("source %s/%s", path_here, path))
-end
+local vscode_exists = vim.fn.exists('g:vscode') == 1
+local source = function(path) vim.cmd(string.format("source %s/%s", path_here, path)) end
 
 require('custom.general_options')
 set_general_options()
 
-if vscode_exists() then
+if vscode_exists then
     -- Running inside vscode
 else
     require('custom.options')
--- nvim/lua/dein.lua
+++ nvim/lua/dein.lua
@@ -4,24 +4,21 @@ local add = fn['dein#add']
 local begin = fn['dein#begin']
 local check_install = fn['dein#check_install']
 local install = fn['dein#install']
+local load_toml = fn['dein#load_toml']
 local end_ = fn['dein#end']
 
 function activate_dein(path)
-    -- Required:
-    -- Add the dein installation directory into runtimepath
     local dein_path = string.format("%s/dein", path)
     set.runtimepath:append(dein_path)
 
-    -- Required:
     local cache_path = string.format("%s/../../.local/share/dein", path)
     begin(cache_path)
-
-    -- Let dein manage dein
     add(dein_path)
-
-    -- Required:
+    local toml_path = string.format("%s/dein.toml", path)
+    load_toml(toml_path)
+    local lazy_toml_path = string.format("%s/dein_lazy.toml", path)
+    load_toml(lazy_toml_path, { lazy = 1 })
     end_()
 
-    -- If you want to install not installed plugins on startup.
     if check_install() == 1 then install() end
 end
