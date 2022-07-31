d3a6480e8fb4f703889197c5397d0d732af65b1d
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-07-31 21:51:41 +0800

    nlua&C-J,C-K choose suggestions

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -1,5 +1,9 @@
+local pumvisible = function() return vim.fn.pumvisible == 1 end
+
 function set_keymap()
     local set = vim.keymap.set
     set('i', 'jk', '<Esc>')
     set('i', 'kj', '<Esc>')
+    set('i', '<C-J>', function() if pumvisible() then return '<C-J>' else return '<C-N>' end end, { expr = true })
+    set('i', '<C-K>', function() if pumvisible() then return '<C-K>' else return '<C-P>' end end, { expr = true })
 end
