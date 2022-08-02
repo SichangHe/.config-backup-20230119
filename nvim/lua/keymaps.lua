df6281842835f0721fd2ceea541419fc3c065292
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-08-02 09:20:20 +0800

    coc settings and format key

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -3,6 +3,10 @@ local suggestion_show = function() return vim.fn.pumvisible == 1 end
 local suggestion_selected = function() return vim.fn.empty('completion_selected') == 1 end
 local set = vim.keymap.set
 
+function Keymaps.format()
+    vim.cmd('CocCommand editor.action.formatDocument')
+end
+
 function Keymaps.set()
     set('i', 'jk', '<Esc>')
     set('i', 'kj', '<Esc>')
@@ -10,7 +14,8 @@ function Keymaps.set()
     set('i', '<C-K>', function() return suggestion_show() and '<C-K>' or '<C-P>' end, { expr = true })
     set('n', '<Space>w', '<C-W>')
     set('n', '<Space>p', ':CocList commands<CR>')
-    set('n', '<Space>f', ':CocCommand editor.action.formatDocument<CR>')
+    set('n', '<Space>f', ':lua Keymaps.format()<CR>')
+    set('n', '<Space>s', ':w<CR>:lua Keymaps.format()<CR>')
 end
 
 return Keymaps
