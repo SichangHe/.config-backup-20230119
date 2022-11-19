6d8647d40f30f7d0e4bd1d2f79656f675be53229
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-11-19 09:28:43 -0500

    fugitive&add things around selection

--- nvim/lua/general_keymaps.lua
+++ nvim/lua/general_keymaps.lua
@@ -3,6 +3,12 @@ local key = vim.keymap.set
 
 function M.set()
     key('n', '<Space>n', ':noh<CR>')
+    key('v', [[<Space>(]], [[c(<C-r>*)<Esc>]])
+    key('v', [[<Space>[]], [[c[<C-r>*]<Esc>]])
+    key('v', [[<Space>{]], [[c{<C-r>*}<Esc>]])
+    key('v', [[<Space>`]], [[c`<C-r>*`<Esc>]])
+    key('v', [[<Space>']], [[c'<C-r>*'<Esc>]])
+    key('v', [[<Space>"]], [[c"<C-r>*"<Esc>]])
 end
 
 return M
