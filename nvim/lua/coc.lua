f8dfceed7e166ad66fcd213f1a56dddaf742bce1
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-12-02 11:59:13 -0500

    rm useless coc restart logic

--- nvim/lua/coc.lua
+++ nvim/lua/coc.lua
@@ -1,7 +1,5 @@
 local M = {}
 local key = vim.keymap.set
-local set = vim.opt
-local cmd = vim.cmd
 local fn = vim.fn
 local pum_visible = function()
     return fn['coc#pum#visible']() == 1
@@ -11,17 +9,8 @@ local fn_async = function(action)
     return function() async(action) end
 end
 local refresh = fn['coc#refresh']
-M.path_len = #set.runtimepath:get()
 local create = function(event, command)
     vim.api.nvim_create_autocmd(event, {command = command})
-end
-
-function M.new_file_open()
-    local new_path_len = #set.runtimepath:get()
-    if new_path_len ~= M.path_len then
-        M.path_len = new_path_len
-        cmd('silent! CocRestart')
-    end
 end
 
 function M.setup()
@@ -42,9 +31,7 @@ function M.setup()
     key('i', '<C-k>', function()
         return '<C-g>u' .. (pum_visible() and fn['coc#pum#prev'](1) or refresh())
     end, {expr = true})
-    create('DirChanged', 'lua Coc.new_file_open()') -- Auto reload Coc on changing directory.
     create('CursorHold', "lua Coc.async('highlight')")
-    create('CmdLineEnter', 'lua Coc.path_len = #vim.opt.runtimepath:get()') -- Update `Coc.path_len` on any command.
 end
 
 M.async = async
