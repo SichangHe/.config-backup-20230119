fd5accaaceb47a93868542c72d682bad5312a69d
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-12-13 08:22:37 -0500

    nvim right mouse extend

--- nvim/lua/general_options.lua
+++ nvim/lua/general_options.lua
@@ -4,6 +4,7 @@ local set = vim.opt
 function M.set()
     vim.cmd("lang en_US.UTF-8")
     set.clipboard = 'unnamed'
+    set.mousemodel = 'extend'
     set.timeoutlen = 300
     set.undofile = true
     set.virtualedit = 'block'
