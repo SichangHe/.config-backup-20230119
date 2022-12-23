868b75c55680bc176d49fceb7c17b9b03ef0bbe6
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-12-22 20:30:56 -0500

    lualine relative file name

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -27,7 +27,7 @@ return function(use)
                 },
                 sections = {
                     lualine_a = { 'mode' },
-                    lualine_b = { 'filename' },
+                    lualine_b = {{ 'filename', path = 1 }},
                     lualine_c = { 'diagnostics' },
                     lualine_x = { 'filetype' },
                     lualine_y = { 'progress', 'location', 'diff' },
